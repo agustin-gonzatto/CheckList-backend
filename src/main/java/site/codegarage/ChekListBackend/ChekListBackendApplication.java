a0fe62dba25942d3f78975bf5183ecd8bec9fe6d
Agustin <gonzattoagustin1@gmail.com>
2025-03-03 23:22:29 -0300

    Add spring project

---
+++ src/main/java/site/codegarage/ChekListBackend/ChekListBackendApplication.java
@@ -0,0 +1,13 @@
+package site.codegarage.ChekListBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChekListBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChekListBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/codegarage/ChekListBackend/ChekListBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package site.codegarage.ChekListBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChekListBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

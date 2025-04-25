beec598f12d9a32293343c99a74167e4712c6f75
Agustin <gonzattoagustin1@gmail.com>
2025-04-25 11:09:13 -0300

    ,

--- src/main/java/site/codegarage/CheckListBackend/config/SecurityConfig.java
+++ src/main/java/site/codegarage/CheckListBackend/config/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:5500"));
-        configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE", "OPTIONS","PATCH"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
         configuration.setAllowCredentials(true);
 
